ff8a072ee57095efb097bf8b9afdb7d414e28d35
RyanBAtk\ryanb <ryanb.atk@gmail.com>
2019-12-04 16:53:54 -0600

    Revert "Resolved issue with being unable to find path"
    This reverts commit 195e60535d74f0cc792f649ec2e62c6b214a50b3.

--- RestaurantApp/views.py
+++ RestaurantApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.http import HttpResponse
 import json
 from . import searchfunctions
 
@@ -8,16 +7,6 @@ from . import searchfunctions
 # Create your views here.
 
 def findfood(req):
-    params = HttpRequest.content_params #this is a dictionary
-    
-    
-    
-def ping_request(req):
-    print("ping request received from:", req.META.get("REMOTE_ADDR"))
-    html = "<html><body>The server has been pinged.</body></html>"
-    return HttpResponse(html)
-    
-def findfood_old(req):
     req_params = json.loads(req.body.decode('utf-8'))
 
     coord = dict(location=dict(lat=req_params['data']['long'], lng=req_params['data']['lat']))
